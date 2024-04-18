ee6665cd46d66c0823d817573ee34d62f09c58b9
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-18 16:07:07 -0300

    Transferindo código do botão e usando CSS global

--- libs/botao/src/lib/botao/botao.component.stories.ts
+++ libs/botao/src/lib/botao/botao.component.stories.ts
@@ -2,12 +2,101 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { BotaoComponent } from './botao.component';
 
 const meta: Meta<BotaoComponent> = {
+  title: 'botao',
   component: BotaoComponent,
-  title: 'BotaoComponent',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Este é um componente de botão utilizado para disparar ações.'
+      }
+    },
+  }
 };
 export default meta;
 type Story = StoryObj<BotaoComponent>;
 
-export const Primary: Story = {
-  args: {},
+export const Primario: Story = {
+  args: {
+    texto: 'Action',
+    desabilitado: false,
+    variante: 'primario',
+    tema: 'azul'
+  }
+};
+
+export const PrimarioDesabilitado: Story = {
+  args: {
+    ...Primario.args,
+    desabilitado: true,
+  }
+}
+
+export const Secundario: Story = {
+  args: {
+    ...Primario.args,
+    variante: 'secundario',
+  }
+};
+
+export const SecundarioDesabilitado: Story = {
+  args: {
+    ...Secundario.args,
+    desabilitado: true,
+  }
+}
+
+export const Terciario: Story = {
+  args: {
+    ...Primario.args,
+    variante: 'terciario',
+  }
+};
+
+export const TerciarioDesabilitado: Story = {
+  args: {
+    ...Terciario.args,
+    desabilitado: true,
+  }
+};
+
+export const PrimarioVioleta: Story = {
+  args: {
+    ...Primario.args,
+    tema: 'violeta'
+  }
+};
+
+export const PrimarioVioletaDesabilitado: Story = {
+  args: {
+    ...PrimarioVioleta.args,
+    desabilitado: true
+  }
+};
+
+export const SecundarioVioleta: Story = {
+  args: {
+    ...PrimarioVioleta.args,
+    variante: 'secundario',
+  }
+};
+
+export const SecundarioVioletaDesabilitado: Story = {
+  args: {
+    ...SecundarioVioleta.args,
+    desabilitado: true,
+  }
+};
+
+export const TerciarioVioleta: Story = {
+  args: {
+    ...PrimarioVioleta.args,
+    variante: 'terciario',
+  }
+};
+
+export const TerciarioVioletaDesabilitado: Story = {
+  args: {
+    ...TerciarioVioleta.args,
+    desabilitado: true,
+  }
 };
--- libs/botao/src/lib/botao/botao.component.ts
+++ libs/botao/src/lib/botao/botao.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+type variantes = 'primario' | 'secundario' | 'terciario';
+type temas = 'azul' | 'violeta';
 
 @Component({
   selector: 'lib-botao',
@@ -8,4 +11,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './botao.component.html',
   styleUrl: './botao.component.css',
 })
-export class BotaoComponent {}
+export class BotaoComponent {
+  /**
+   * Texto botão
+   */
+  @Input() texto = '';
+  @Input() desabilitado = false;
+  @Input() variante: variantes = 'primario';
+  @Input() tema: temas = 'azul';
+
+  obterVariante(variante: variantes, desabilitado: boolean, tema: temas): string {
+    let classes = `${variante} ${tema}`;
+
+    classes += desabilitado ? " disabled" : "";
+
+    return classes;
+  }
+}
