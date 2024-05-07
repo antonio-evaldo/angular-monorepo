061de1299df8f82901baf3a9ed2c2f1cf4e0d01b
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-07 11:49:25 -0300

    Transferindo código de botao para button

--- libs/button/src/lib/button/button.component.ts
+++ libs/button/src/lib/button/button.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+type variantes = 'primario' | 'secundario' | 'terciario';
+type temas = 'azul' | 'violeta';
 
 @Component({
   selector: 'lib-button',
@@ -8,4 +11,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+
+  /**
+   * Texto botão
+   */
+  @Input() texto = '';
+  @Input() desabilitado = false;
+  @Input() variante: variantes = 'primario';
+  @Input() tema: temas = 'azul';
+
+  obterVariante(variante: variantes, desabilitado: boolean, tema: temas): string {
+    let classes = `${variante} ${tema}`;
+
+    classes += desabilitado ? " disabled" : "";
+
+    return classes;
+  }
+}
--- libs/storybook-host/src/stories/button.component.stories.ts
+++ libs/storybook-host/src/stories/button.component.stories.ts
@@ -3,11 +3,101 @@ import { ButtonComponent } from '../../../button/src/lib/button/button.component
 
 const meta: Meta<ButtonComponent> = {
   component: ButtonComponent,
-  title: 'ButtonComponent',
+  title: 'Button',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Este é um componente de botão utilizado para disparar ações.'
+      }
+    },
+  }
 };
+
 export default meta;
 type Story = StoryObj<ButtonComponent>;
 
-export const Primary: Story = {
-  args: {},
+export const Primario: Story = {
+  args: {
+    texto: 'Action',
+    desabilitado: false,
+    variante: 'primario',
+    tema: 'azul'
+  }
+};
+
+export const PrimarioDesabilitado: Story = {
+  args: {
+    ...Primario.args,
+    desabilitado: true,
+  }
+}
+
+export const Secundario: Story = {
+  args: {
+    ...Primario.args,
+    variante: 'secundario',
+  }
+};
+
+export const SecundarioDesabilitado: Story = {
+  args: {
+    ...Secundario.args,
+    desabilitado: true,
+  }
+}
+
+export const Terciario: Story = {
+  args: {
+    ...Primario.args,
+    variante: 'terciario',
+  }
+};
+
+export const TerciarioDesabilitado: Story = {
+  args: {
+    ...Terciario.args,
+    desabilitado: true,
+  }
+};
+
+export const PrimarioVioleta: Story = {
+  args: {
+    ...Primario.args,
+    tema: 'violeta'
+  }
+};
+
+export const PrimarioVioletaDesabilitado: Story = {
+  args: {
+    ...PrimarioVioleta.args,
+    desabilitado: true
+  }
+};
+
+export const SecundarioVioleta: Story = {
+  args: {
+    ...PrimarioVioleta.args,
+    variante: 'secundario',
+  }
+};
+
+export const SecundarioVioletaDesabilitado: Story = {
+  args: {
+    ...SecundarioVioleta.args,
+    desabilitado: true,
+  }
+};
+
+export const TerciarioVioleta: Story = {
+  args: {
+    ...PrimarioVioleta.args,
+    variante: 'terciario',
+  }
+};
+
+export const TerciarioVioletaDesabilitado: Story = {
+  args: {
+    ...TerciarioVioleta.args,
+    desabilitado: true,
+  }
 };
