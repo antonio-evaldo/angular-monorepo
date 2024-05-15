6a1f3a5f5050249b7b1eaa20a68459995af331b3
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-05-15 14:36:14 -0300

    chore(release): publish 0.3.0

---
+++ libs/teste/src/lib/teste/teste.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'alfabit-teste',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './teste.component.html',
+  styleUrl: './teste.component.css',
+})
+export class TesteComponent {}
